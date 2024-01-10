06b2a3dfe85296e7f18dffbaeba5e54b0218c7ea
avafaisal <73430884+avafaisal@users.noreply.github.com>
2024-01-10 10:16:33 +0700

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ const port = process.env.PORT || 5000;
 const DB_CONNECTION = process.env.DB_CONNECTION;
 
 app.use(morgan('dev'));
-app.use(express.static("public"));
+// app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
 app.use(FileUpload());
 app.use(cors());
 app.use(bodyParser.json());
