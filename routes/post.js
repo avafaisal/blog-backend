f252694607641b0e563ecd1f0451031835148d1a
avafaisal <73430884+avafaisal@users.noreply.github.com>
2024-01-10 12:29:00 +0700

    Update post.js

--- routes/post.js
+++ routes/post.js
@@ -1,27 +1,29 @@
 const express = require("express");
-const multer = require('multer');
+const multer = require("multer");
 
 const router = express.Router();
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'public/images/') // Direktori penyimpanan
-    },
-    filename: function (req, file, cb) {
-      cb(null, Date.now() + '-' + file.originalname)
-    }
-  });
-  
-const upload = multer({ storage: storage });
+  destination: function (req, file, cb) {
+    cb(null, "public/images/"); // Direktori penyimpanan
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({
+  storage: storage,
+});
 
 const Post = require("../controllers/Post");
 
 router.get("/", Post.getPosts);
 router.get("/:id", Post.getPostById);
 router.get("/categoryname/:name", Post.getPostByCategoryName);
-router.post("/", upload.single('image'), Post.createPost);
-router.put("/:id", Post.updatePost);
-router.patch("/:id", Post.updatePost);
+router.post("/", upload.single("image"), Post.createPost);
+router.put("/:id", upload.single("image"), Post.updatePost);
+router.patch("/:id", upload.single("image"), Post.updatePost);
 router.delete("/:id", Post.deletePost);
 
 module.exports = router;
