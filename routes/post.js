50d3b102f7ec6712f71234b4752b6f0e0f370e4e
avafaisal <73430884+avafaisal@users.noreply.github.com>
2024-01-10 20:49:43 +0700

    Update post.js

--- routes/post.js
+++ routes/post.js
@@ -6,7 +6,7 @@ const router = express.Router();
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     // cb(null, "public/images/"); 
-    cb(null, 'tmp');
+    cb(null, "tmp/");
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + "-" + file.originalname);
