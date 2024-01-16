0e668d59cfd2995a650e9056b3660ea15c5b03a4
avafaisal <73430884+avafaisal@users.noreply.github.com>
2024-01-16 07:24:41 +0700

    Update post.js

--- routes/post.js
+++ routes/post.js
@@ -1,30 +1,15 @@
 const express = require("express");
-const multer = require("multer");
 
 const router = express.Router();
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    // cb(null, "public/images/"); 
-    cb(null, "/tmp");
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalname);
-  },
-});
-
-const upload = multer({
-  storage: storage,
-});
 
 const Post = require("../controllers/Post");
 
 router.get("/", Post.getPosts);
 router.get("/:id", Post.getPostById);
 router.get("/categoryname/:name", Post.getPostByCategoryName);
-router.post("/", upload.single("image"), Post.createPost);
-router.put("/:id", upload.single("image"), Post.updatePost);
-router.patch("/:id", upload.single("image"), Post.updatePost);
+router.post("/", Post.createPost);
+router.put("/:id", Post.updatePost);
+router.patch("/:id", Post.updatePost);
 router.delete("/:id", Post.deletePost);
 
 module.exports = router;
