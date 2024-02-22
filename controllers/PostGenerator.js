734ae34909cf9c9d85da639216b17aa92b1f8427
avafaisal <73430884+avafaisal@users.noreply.github.com>
2024-02-22 08:48:44 +0700

    Update PostGenerator.js

--- controllers/PostGenerator.js
+++ controllers/PostGenerator.js
@@ -2,41 +2,24 @@ const Post = require("../models/Post");
 const slugify = require("slugify");
 const axios = require("axios");
 
-const trim_words = (theString, numWords) => {
-  expString = theString.split(/\s+/, numWords);
-  theNewString = expString.join(" ");
-  return theNewString;
-};
-
-// SCRAPE DATA
 const postGenerator = async (req, res) => {
-  let keyword = req.body.keyword;
-  let size = req.body.size;
+  let title = req.body.title;
   let category = req.body.category;
 
   await axios
-    .get(
-      `https://search.api.cnn.io/content?q=${keyword}&size=${size}&sort=relevance`
-    )
+    .post("https://prickly-bikini-ray.cyclic.app/generate", { title })
     .then((result) => {
-      data = result.data.result;
-
-      for (i = 0; i < data.length; i++) {
-        let headline = data[i]["headline"].replace(/[^a-zA-Z0-9 ]/g, "");
-        let content = data[i]["body"].split(".").join(".<br><br>");
-        const createPost = new Post({
-          title: headline,
-          content: content,
-          description: trim_words(data[i]["body"], 20),
-          category: category,
-          slug: slugify(headline),
-          imageUrl: data[i]["thumbnail"],
-        });
-        createPost.save();
-      }
-
-      // Success
-      res.json({ message: "Post genarator successfully" });
+      const createPost = new Post({
+        title: title,
+        content: result.data.content,
+        description: result.data.description,
+        category: category,
+        slug: slugify(title),
+        image: result.data.image,
+        imageUrl: result.data.image,
+      });
+      createPost.save();
+      res.status(201).json({ message: "Post genarator successfully" });
     })
     .catch((error) => {
       res.json({ message: error.message });
